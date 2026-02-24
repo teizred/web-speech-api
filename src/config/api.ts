fd3a93df1e26ba1fa2b7bc8029f433aff39ffa2d
teizred <91955720+teizred@users.noreply.github.com>
2026-02-24 11:13:59 +0100

    chore: Conditionally set API_BASE_URL to localhost for development or the production URL.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://web-speech-api-backend-production.up.railway.app";
+export const API_BASE_URL = import.meta.env.DEV ? "http://localhost:3001" : "https://web-speech-api-backend-production.up.railway.app";
